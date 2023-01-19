2da1cff25adf77c8a196062ba2e2c8e97afa0275
Arya Adesh <aryaadesh.cs20@rvce.edu.in>
2023-01-19 18:15:14 +0530

    Add Manual Entry Script

---
+++ JS/quiz_script - Copy.js
@@ -0,0 +1,154 @@
+
+// Item Controller
+const ItemCtrl = (function(){
+  // Item Constructor
+  const Item = function(id, name, calories){
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
+
+  // Data Structure / State
+  const data = {
+    items: StorageCtrl.getItemsFromStorage(),
+    currentItem: null,
+    totalCalories: 0
+  }
+
+  // Public methods
+  return {
+    getItems: function(){
+      return data.items;
+    },
+    addItem: function(name, calories){
+      let ID;
+      // Create ID
+      if(data.items.length > 0){
+        ID = data.items[data.items.length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Calories to number
+      calories = parseInt(calories);
+
+      // Create new item
+      newItem = new Item(ID, name, calories);
+
+      // Add to items array
+      data.items.push(newItem);
+
+      var str=name+" "+calories+" grams";
+
+  const data2 = {
+  name: str
+};
+
+// Options for the request
+const options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(data2)
+};
+
+// Make the request
+fetch("/manual", options)
+  .then(response => {
+    console.log(response.status);
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      
+      throw new Error("Error: " + response.status);
+    }
+  })
+  .then(data3 => {
+    // Do something with the response data
+    console.log(data3);
+  })
+  .catch(error => {
+  
+    console.error(error);
+    if (error.message.includes("500")) {
+      // Show a window alert for status 500 error
+      alert("An internal server error occurred (Status 500). Please try again later.");
+    } else {
+      // Show a window alert for other errors
+      alert("An error occurred. Please try again later.");
+    }
+  });
+
+
+
+      return newItem;
+},
+    getItemById: function(id){
+      let found = null;
+      // Loop through items
+      data.items.forEach(function(item){
+        if(item.id === id){
+          found = item;
+        }
+      });
+      return found;
+    },
+    updateItem: function(name, calories){
+      // Calories to number
+      calories = parseInt(calories);
+
+      let found = null;
+
+      data.items.forEach(function(item){
+        if(item.id === data.currentItem.id){
+          item.name = name;
+          item.calories = calories;
+          found = item;
+        }
+      });
+      return found;
+    },
+    deleteItem: function(id){
+      // Get ids
+      const ids = data.items.map(function(item){
+        return item.id;
+      });
+
+      // Get index
+      const index = ids.indexOf(id);
+
+      // Remove item
+      data.items.splice(index, 1);
+    },
+    clearAllItems: function(){
+      data.items = [];
+    },
+    setCurrentItem: function(item){
+      data.currentItem = item;
+    },
+    getCurrentItem: function(){
+      return data.currentItem;
+    },
+    getTotalCalories: function(){
+      let total = 0;
+
+      // Loop through items and add cals
+      data.items.forEach(function(item){
+        total += item.calories;
+      });
+
+      // Set total cal in data structure
+      data.totalCalories = total;
+
+      // Return total
+      return data.totalCalories;
+    },
+    logData: function(){
+      return data;
+    }
+  }
+})();
+
+
+
